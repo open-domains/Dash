5c4dbf3283f6d952778a376868050746f8a4e2d4
andrewstech <andrew@win11react.com>
2024-04-06 19:32:13 +0000

    Update admin and user views

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,19 @@ const ENDPOINT = process.env.ENDPOINT;
 module.exports = async (req, res) => {
     const user = req.user;
     const token = req.cookies.token;
-    const data = ''
+    let data = ''
     if (!user.admin){
         return res.redirect("/domains?code=5");
     }
-    res.render("admin", {user: user, domains: data, message: ''})
+    try {
+        const response = await fetch(`${ENDPOINT}/users`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': token
+            }
+        });
+        data = await response.json();
+    res.render("admin", {user: user, users: data, message: ''})
 }
     
